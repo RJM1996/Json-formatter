1aa3207ac4e6609d6e0999d73caf9d90795dc730
wanjie <rongjunming.rjm@alibaba-inc.com>
2024-12-31 16:39:28 +0800

    refactor: 更新构建配置并调整输出目录

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  transpilePackages: ['@monaco-editor/react'],
-}
+  transpilePackages: ["@monaco-editor/react"],
+  webpack: (config) => {
+    config.optimization.minimize = true;
+    return config;
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
