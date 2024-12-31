ce309ba83b0d82a0b8ff681db0369ef859ce554c
wanjie <rongjunming.rjm@alibaba-inc.com>
2024-12-31 17:44:34 +0800

    style: 调整 .gitignore 文件格式

---
+++ out/_next/static/FlyhefiXVkKr7W4vBV1id/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/612-61e02dbcc507ceaa.js","static/chunks/pages/index-e021f20e0b70e480.js"],"/_error":["static/chunks/pages/_error-65ac7445167ddb62.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
